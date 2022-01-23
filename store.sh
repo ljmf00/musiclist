1a3fda5c93913c6a2f5c3d2ecd78e8e43f6ef316
Luís Ferreira <contact@lsferreira.net>
2022-01-23 20:48:35 +0000

    fix(store.sh): keep spotdl cache and fix store lookup
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- store.sh
+++ store.sh
@@ -3,10 +3,14 @@
 set -eu -o pipefail
 
 mkdir -p ./prepare-tmp/
+if [ -f .spotdl-cache ]; then
+  mv .spotdl-cache ./prepare-tmp/
+fi
 spotdl "$@" --output-format mp3 --dt 8 --st 8 -o ./prepare-tmp/
 
 mkdir -p ./prepare/
 mv ./prepare-tmp/*.mp3 ./prepare/
+mv ./prepare-tmp/.spotdl-cache .
 rm -rf ./prepare-tmp/
 
 touch store-cids
@@ -19,7 +23,7 @@ shopt -s dotglob;
 for file in ./prepare/* ; do
   echo "$file";
   FILE_CID_IPFS="$(ipfs add -n --pin=false --cid-version 1 -q "$file")"
-  if ! find ./store/ -name "$FILE_CID_IPFS"; then
+  if find ./store/ -name "$FILE_CID_IPFS" -exec false {} +; then
     exiftool -json "$file" > "./store/all/$FILE_CID_IPFS"
   fi
   rm "$file"
