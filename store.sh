c8da005a5eb9322ef6a5240fe4af64a066528914
Luís Ferreira <contact@lsferreira.net>
2022-01-24 03:57:59 +0000

    fix(store.sh): reflect new store location
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- store.sh
+++ store.sh
@@ -14,23 +14,23 @@ mv ./prepare-tmp/*.mp3 ./prepare/
 mv ./prepare-tmp/.spotdl-cache .
 rm -rf ./prepare-tmp/
 
-mkdir -p ./store/temp/
+mkdir -p ./store/metadata/temp/
 
 shopt -s dotglob;
 for file in ./prepare/* ; do
   FILE_CID_IPFS="$(ipfs add -n --pin=false --cid-version 1 -q "$file")"
   echo "$FILE_CID_IPFS"
-  if find ./store/ -type f -name "$FILE_CID_IPFS" -exec false {} +; then
-    exiftool -json "$file" > "./store/temp/$FILE_CID_IPFS"
+  if find ./store/metatada/ -type f -name "$FILE_CID_IPFS" -exec false {} +; then
+    exiftool -json "$file" > "./store/metadata/temp/$FILE_CID_IPFS"
   else
-    ln -s "$(find ./store/ -type f -name "$FILE_CID_IPFS" | sed 's|^\.\/store|\.\.|')" "./store/temp/$FILE_CID_IPFS"
+    ln -s "$(find ./store/metadata/ -type f -name "$FILE_CID_IPFS" | sed 's|^\.\/store\/metadata|\.\.|')" "./store/metadata/temp/$FILE_CID_IPFS"
     rm "$file"
   fi
 done
 
-touch store-cids
+touch ./store/cids
 node index.js ./prepare/ > store.log
-cat store.log >> store-cids
+cat store.log >> ./store/cids
 
 for file in ./prepare/* ; do
   rm "$file"
