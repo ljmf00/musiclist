04e091a3aa0cb4038e26c384f40a92a7bfeb4efe
Luís Ferreira <contact@lsferreira.net>
2022-01-25 18:45:40 +0000

    fix(store.sh): typo on metadata folder
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- store.sh
+++ store.sh
@@ -20,7 +20,7 @@ shopt -s dotglob;
 for file in ./prepare/* ; do
   FILE_CID_IPFS="$(ipfs add -n --pin=false --cid-version 1 -q "$file")"
   echo "$FILE_CID_IPFS"
-  if find ./store/metatada/ -type f -name "$FILE_CID_IPFS" -exec false {} +; then
+  if find ./store/metadata/ -type f -name "$FILE_CID_IPFS" -exec false {} +; then
     exiftool -json "$file" > "./store/metadata/temp/$FILE_CID_IPFS"
   else
     ln -s "$(find ./store/metadata/ -type f -name "$FILE_CID_IPFS" | sed 's|^\.\/store\/metadata|\.\.|')" "./store/metadata/temp/$FILE_CID_IPFS"
