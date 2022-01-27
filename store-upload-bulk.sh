ba0670b89ac562c48e565b78d1848a2760c1e905
Luís Ferreira <contact@lsferreira.net>
2022-01-27 19:06:44 +0000

    feat(scripts): add script to store and upload in bulk
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

---
+++ store-upload-bulk.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+
+for folder in */ ; do
+  if [[ "$folder" == "prepare-"[0-9]* ]]; then
+    echo "Processing ${folder%/}";
+    if [ -d "prepare" ] && (find prepare -type f | grep ''); then
+      echo "!! Folder contains stuff, hanging."
+      exit 1
+    fi
+
+    rm -rf prepare
+    mv "${folder%/}" prepare
+    ./store.sh
+    ./upload.sh
+  fi
+done
+
