64e8322a7e4f101db2a5b55092e9286ed33d7948
Luís Ferreira <contact@lsferreira.net>
2022-01-28 00:49:42 +0000

    chore(scripts): pipe to null device to avoid useless logging
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- store-upload-bulk.sh
+++ store-upload-bulk.sh
@@ -5,7 +5,7 @@ set -eu -o pipefail
 for folder in */ ; do
   if [[ "$folder" == "prepare-"[0-9]* ]]; then
     echo "Processing ${folder%/}";
-    if [ -d "prepare" ] && (find prepare -type f | grep ''); then
+    if [ -d "prepare" ] && (find prepare -type f | grep '' >/dev/null); then
       echo "!! Folder contains stuff, hanging."
       exit 1
     fi
