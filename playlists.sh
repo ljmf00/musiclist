64527fa31a39c1f37762accf6ad0bbe7412a40ab
Luís Ferreira <contact@lsferreira.net>
2022-01-28 05:13:55 +0000

    fix(playlists): delete extra files on rsync destination
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- playlists.sh
+++ playlists.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ./mkplaylists.d --localhost
-rsync -avh --progress "playlists/" "$HOME/Sync/playlists/local-store/"
+rsync -avh --delete --progress "playlists/" "$HOME/Sync/playlists/local-store/"
 ./mkplaylists.d
-rsync -avh --progress "playlists/" "$HOME/Sync/playlists/store/"
+rsync -avh --delete --progress "playlists/" "$HOME/Sync/playlists/store/"
 
