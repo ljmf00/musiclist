3012116ba6d470604113d67693ca088ca45cc972
Luís Ferreira <contact@lsferreira.net>
2022-01-28 00:50:15 +0000

    fix(upload.sh): avoid running nodejs when there is nothing inside prepare
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- upload.sh
+++ upload.sh
@@ -2,11 +2,13 @@
 
 set -eu -o pipefail
 
-touch ./store/cids
-node index.js ./prepare/ > store.log
-cat store.log >> ./store/cids
+if [ -d "prepare" ] && (find prepare -type f | grep '' >/dev/null); then
+  touch ./store/cids
+  node index.js ./prepare/ > store.log
+  cat store.log >> ./store/cids
 
-for file in ./prepare/* ; do
-  rm "$file"
-done
+  for file in ./prepare/* ; do
+    rm "$file"
+  done
+fi
 
