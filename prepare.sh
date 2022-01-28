aa2132f246293e3e0b593bed638f9ee23defe46e
Luís Ferreira <contact@lsferreira.net>
2022-01-28 04:16:17 +0000

    feat(prepare.sh): hang when prepare folder has files
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- prepare.sh
+++ prepare.sh
@@ -2,6 +2,11 @@
 
 set -eu -o pipefail
 
+if [ -d "prepare" ] && (find prepare -type f | grep '' >/dev/null); then
+  echo "!! Folder contains stuff, hanging."
+  exit 1
+fi
+
 ./download.sh "$@"
 ./store.sh
 ./upload.sh
