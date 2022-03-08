3b1f75fc8fbdaafb6e51219c17eabbcee822272c
Luís Ferreira <contact@lsferreira.net>
2022-03-08 16:32:27 +0000

    feat(scipts): add shuffle capability to store-pin.sh script
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- store-pin.sh
+++ store-pin.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-for blob in $(./store-ls.sh); do
+if [[ "$1" == "shuffle " ]]; then
+  FILES="$(./store-ls.sh | shuffle)"
+else
+  FILES="$(./store-ls.sh)"
+fi
+
+for blob in $FILES; do
   ipfs pin add "$blob" --progress;
 done
