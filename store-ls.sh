fc195d667b6c04947650570b1a62f908a3df64e3
Luís Ferreira <contact@lsferreira.net>
2022-04-22 17:46:25 +0100

    fix(treewide): remove code smells reported by pre-commit
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- store-ls.sh
+++ store-ls.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+# Garanteed to contain no spaces, no need to use while read
+#shellcheck disable=SC2013
 (for blob in $(cat store/cids); do ipfs ls -s "/ipfs/$blob/prepare"; done) > store/cids-ls
 
+#shellcheck disable=SC2002
 comm -1 -2 \
   <(find store/metadata/ -type f -print0 | xargs -0 -I {} basename {} | sort) \
   <(cat store/cids-ls | cut -d' ' -f1 | sort)
